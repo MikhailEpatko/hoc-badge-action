0ffc64d8734478e769babe3fa92ac5d2df608a76
Mikhail Epatko <m.epatko@jsa-group.ru>
2024-03-03 19:38:11 +0300

    fixed parameters check

--- generate-badge.sh
+++ generate-badge.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo "|| $1 || $2 || $3 || $4 ||"
-if [ "$1" == '[]' ]; then Before="$(date +%F)"; fi
-if [ "$2" == '[]' ]; then Dir='.'; fi
-if [ "$3" == '[]' ]; then Exclude=[]; fi
-if [ "$4" == '[]' ]; then Since='2000-01-01'; fi
+if [ "$1" == '[]' ]; then Before="$(date +%F)"; else Before=$1; fi
+if [ "$2" == '[]' ]; then Dir='.'; else Dir=$2; fi
+if [ "$3" == '[]' ]; then Exclude=[]; else Exclude=$3; fi
+if [ "$4" == '[]' ]; then Since='2000-01-01'; else Since=$4; fi
 
 echo "hoc -d $Dir -e $Exclude -s $Since -b $Before -f 'int'"
 Count=$(hoc -d $Dir -e "$Exclude" -s $Since -b $Before -f 'int' )
