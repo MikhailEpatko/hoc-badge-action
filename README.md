f511e8856ecb7e381a437636e56e92d62cbab85e
Mikhail Epatko <m.epatko@jsa-group.ru>
2024-03-03 19:12:15 +0300

    Initial commit. Script can calculate hits

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # hits-of-code-badge
-GitHub action with hoc
+GitHub action with hoc.
+**hoc** - It is a command line tool to calculate Hits-of-Code metric in a source code repository (at the moment it supports Git 2+ and Subversion 1.7+). 
+ You can read more about Hits-of-Code metric in this blog post: Hits-of-Code Instead of SLoC.
+
+[**hoc** project page](https://github.com/yegor256/hoc/tree/master)
+
---
+++ generate-badge.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ "$1" == '[]' ]; then Before="$(date +%F)"; fi
+if [ "$2" == '[]' ]; then Dir='.'; fi
+if [ "$3" == '[]' ]; then Exclude=[]; fi
+if [ "$4" == '[]' ]; then Since='2000-01-01'; fi
+
+gem install hoc
+Count=$(hoc -d $Dir -e "$Exclude" -s $Since -b $Before -f 'int' )
+echo "Hits of code: $Count"
+
+mkdir ./output
+pip install anybadge
+anybadge -l "Hits of Code" -v "$Count" -f ./output/hits.svg -c royalblue
