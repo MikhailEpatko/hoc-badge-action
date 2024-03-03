ee72cadc2ac9d99bbaed931c32d1266b479bf190
Mikhail Epatko <m.epatko@jsa-group.ru>
2024-03-03 19:34:42 +0300

    added debug

--- generate-badge.sh
+++ generate-badge.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
+echo "|| $1 || $2 || $3 || $4 ||"
 if [ "$1" == '[]' ]; then Before="$(date +%F)"; fi
 if [ "$2" == '[]' ]; then Dir='.'; fi
 if [ "$3" == '[]' ]; then Exclude=[]; fi
 if [ "$4" == '[]' ]; then Since='2000-01-01'; fi
 
-pwd
-ls
-#gem install hoc
+echo "hoc -d $Dir -e $Exclude -s $Since -b $Before -f 'int'"
 Count=$(hoc -d $Dir -e "$Exclude" -s $Since -b $Before -f 'int' )
 echo "Hits of code: $Count"
 
