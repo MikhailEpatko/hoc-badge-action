90b4a98de4f27260c4a32dc094e2b7e85405232f
Mikhail Epatko <m.epatko@jsa-group.ru>
2024-03-04 20:41:03 +0300

    [i1] Added: output directory and output file configs

--- generate-badge.sh
+++ generate-badge.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 if [ "$1" == '[]' ]; then Before="$(date +%F)"; else Before=$1; fi
-if [ "$2" == '[]' ]; then Dir='.'; else Dir=$2; fi
-if [ "$3" == '[]' ]; then Exclude=[]; else Exclude=$3; fi
-if [ "$4" == '[]' ]; then Since='2000-01-01'; else Since=$4; fi
+Dir=$2
+Exclude=$3
+Since=$4
+OutDir=$5
+Filename=$6
 
-Count=$(hoc -d "$Dir" -e "$Exclude" -s "$Since" -b "$Before" -f 'int' )
+Count=$(hoc -d "$Dir" -e "$Exclude" -s "$Since" -b "$Before" -f "int")
 echo "Hits of code: $Count"
 
-anybadge -l "Hits of Code" -v "$Count" -f ./output/hits.svg -c royalblue
+anybadge -l "Hits of Code" -v "$Count" -f "$OutDir/$Filename" -c royalblue
