42e6d5bb10e7bdbcdd9a9939d37789198ff4b1b0
Mikhail Epatko <m.epatko@jsa-group.ru>
2024-03-06 21:33:04 +0300

    #9 Refactored generate-badge.sh

--- generate-badge.sh
+++ generate-badge.sh
@@ -1,17 +1,44 @@
 #!/bin/bash
 
-if [ "$1" == '[]' ]; then Before="$(date +%F)"; else Before=$1; fi
-Dir=$2
-Exclude=$3
-Since=$4
-OutDir=$5
-Filename=$6
+while getopts 'b:d:e:f:o:s:' opt; do
+  case "$opt" in
+    b)
+      Before="$OPTARG"
+      if [ "$Before" == '[]' ]; then Before="$(date +%F)"; else Before=$1; fi
+      ;;
+    d)
+      Dir="$OPTARG"
+      ;;
+    e)
+      Exclude="$OPTARG"
+      ;;
+    f)
+      Filename="$OPTARG"
+      ;;
+    o)
+      OutDir="$OPTARG"
+      ;;
+    s)
+      Since="$OPTARG"
+      ;;
+    :)
+      echo "Usage: $(basename $0) [-b Before] [-d Dir] [-e Exclude] [-f Filename] [-o OutputDir] [-s Since]"
+      exit 1
+      ;;
+    ?)
+      echo "Usage: $(basename $0) [-b Before] [-d Dir] [-e Exclude] [-f Filename] [-o OutputDir] [-s Since]"
+      exit 1
+      ;;
+  esac
+done
 
-gem install hoc
-pip install anybadge
+if [ "$Before" == '[]' ]; then Before="$(date +%F)"; fi
+
+echo "$Dir ${Exclude[*]} $Since $Before"
+
 mkdir -p "$OutDir"
 
-Count=$(hoc -d "$Dir" -e "$Exclude" -s "$Since" -b "$Before" -f "int")
+Count=$(hoc -d "$Dir" ${Exclude:+"-e" "${Exclude[@]}"} -e "${Exclude[@]}" -s "$Since" -b "$Before" -f "int")
 echo "Hits of code: $Count"
 
 anybadge -l "Hits of Code" -v "$Count" -f "$OutDir/$Filename" -c royalblue
